7ebe1b340f78b42b2680fe7b84f0ea0c2d8d4780
abhi <agrawalabhishek622@gmail.com>
2023-08-30 22:13:59 +0530

    implemented the remove and exit button

--- experiments/gui.py
+++ experiments/gui.py
@@ -7,10 +7,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=function.get_todos(), key='todos',
                       enable_events=True, size=[45, 15])
 edit_button = sg.Button("Edit")
+remove_buton = sg.Button("Remove")
+exit_Button = sg.Button("Exit")
+
 
 window = sg.Window("My Todo App",
                    layout=[[label], [input_box, add_button],
-                           [list_box, edit_button]],
+                           [list_box, edit_button, remove_buton],
+                           [exit_Button]],
                    font=('Helvetica', 16))
 
 while True:
@@ -28,6 +32,7 @@ while True:
 
         case "Edit":
             todo_to_edit = values['todos'][0]
+
             new_todo = values['todo']
 
             todos = function.get_todos()
@@ -35,6 +40,17 @@ while True:
             todos[index] = new_todo
             function.write_todos(todos)
             window['todos'].update(values=todos)
+
+        case "Remove":
+            todo_to_remove = values['todos'][0]
+            todos = function.get_todos()
+            todos.remove(todo_to_remove)
+            function.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+
+        case "Exit":
+            break
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
